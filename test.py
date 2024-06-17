2d8c010266e799c1eae5f1ac18680825d227cfbe
Alex <alex@maniko-nails.com>
2024-06-17 18:06:24 +0200

    u

--- test.py
+++ test.py
@@ -70,3 +70,16 @@ class TestAddToCart(unittest.TestCase):
             return max_requests_before_limit
 if __name__ == '__main__':
     unittest.main()
+
+
+
+    from UTILS.job_module import job  # Ensure job_module.py is properly set up and accessible
+
+def run_job():
+    """
+    Function to be called to execute the job.
+    """
+    job()
+
+if __name__ =='__main__':
+    run_job()
